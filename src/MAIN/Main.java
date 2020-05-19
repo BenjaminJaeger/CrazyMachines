415252e7cc7569f78eed03cfd772466c426aaa1d
patrykwatola <49372816+patrykwatola@users.noreply.github.com>
2020-05-19 15:27:57 +0200

    Aspect Ratio fixed

--- src/MAIN/Main.java
+++ src/MAIN/Main.java
@@ -17,11 +17,20 @@ public class Main extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		StackPane root = new StackPane();
 		
-		primaryStage.setTitle("Editor Alpha");
-		primaryStage.setScene(new Scene(root, 900, 763));
+		primaryStage.setTitle("Editor");
+		primaryStage.setScene(new Scene(root, 700, 830));
 
 		new UI(root, 0);
         new Simulation();
+
+        primaryStage.setMinWidth(700);
+        primaryStage.setMinHeight(830);
+		primaryStage.widthProperty().addListener( e -> {
+			primaryStage.setHeight(primaryStage.getWidth()*1.19);
+		});
+		primaryStage.heightProperty().addListener( e -> {
+			primaryStage.setWidth(primaryStage.getHeight()/1.19);
+		});
 
 		primaryStage.show();
 	}
